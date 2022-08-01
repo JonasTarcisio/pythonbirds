e4fa262c66f072f7d6de4801d3dd6157debf09f6
JonasTarcisio <j.byte@hotmail.com>
2022-08-01 14:29:54 -0300

    criados os metodos de atributos nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,10 @@
 class Pessoa:
+    def __init__(self,nome=None,idade=35):
+        self.idade=idade
+        self.nome=nome
+
+
+
     def cumprimentar(self):
         return f'ola {id(self)}'
 
@@ -6,8 +12,11 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p=Pessoa()
+    p=Pessoa('tarcisio')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-
+    print(p.nome)
+    p.nome='jonas'
+    print(p.nome)
+    print(p.idade)
