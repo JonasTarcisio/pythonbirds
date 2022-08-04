1d2a8f09411626ef3d4daa7be4ab8e10e204aa01
JonasTarcisio <j.byte@hotmail.com>
2022-08-04 20:37:24 -0300

    criada a classe carro

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,7 +10,12 @@ class Pessoa:
     def cumprimentar(self):
         return f'ola {id(self)}'
 
-
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributo_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 
 if __name__ == '__main__':
@@ -31,3 +36,6 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(luciano.olhos)
     print(renzo.olhos)
+
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributo_de_classe(), luciano.nome_e_atributo_de_classe())
